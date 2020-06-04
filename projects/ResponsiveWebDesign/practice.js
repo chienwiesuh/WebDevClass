be5b09f2152ace7582b0e56c633e795c373370f9
chienwiesuh <62032649+chienwiesuh@users.noreply.github.com>
2020-06-04 10:50:25 -0500

    changes

---
+++ projects/ResponsiveWebDesign/practice.js
@@ -0,0 +1,9 @@
+let firstName = prompt('What is your first name?');
+ let firstName.toUpperCase = '';
+ if (firstName === '') {
+     print = 'firstName';
+ } else {
+     print ='Try again';
+ }
+
+ alert(hello);
